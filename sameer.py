b6dfbbb78d7bcc1b482bbbd544c0b86c7343f51a
_XXZZ00ZZXX_ <71839916+UserXTester@users.noreply.github.com>
2021-09-20 12:12:50 +0700

    Update sameer.py
    beta7

--- sameer.py
+++ sameer.py
@@ -8,7 +8,7 @@
 #
 # Created by : https://t.me/AlphaXProject 
 # Support by : https://t.me/CariTemanLink 
-# Version : v3.1.1.11 beta6
+# Version : v3.1.1.11 beta7
 
 
 import os
@@ -238,7 +238,7 @@ async def start_yukki():
     global fvt  
 
 
-    print("\n💥💥 5P4MX UBOT v3.1.1.11 beta6 IS STARTING... 💥💥\n")
+    print("\n💥💥 5P4MX UBOT v3.1.1.11 beta7 IS STARTING... 💥💥\n")
     
     
     if smex:
@@ -3852,7 +3852,7 @@ async def _(event):
             except YouBlockedUserError:
                 await event.edit("`Boss! Please Unblock @SpamBot`")
                 return
-            await event.edit(response.text)
+            await event.edit(f"~ {response.message.message}")
 
 
 # =====[RESTART]=====  
@@ -4214,7 +4214,7 @@ async def help(e):
 For more help regarding usage \nof plugins type plugins name
 
 🤖 𝘽𝙤𝙩 𝙄𝙣𝙛𝙤 
-- version : <code>v3.1.1.11 beta6</code>
+- version : <code>v3.1.1.11 beta7</code>
 - type \t\t: <code>DLX</code>
 - project : <code>@AlphaXProject</code>"""
        await e.reply(text, parse_mode='html', link_preview=None )
@@ -4225,7 +4225,7 @@ For more help regarding usage \nof plugins type plugins name
         
 text = """
 
-💥💥 [CONGRATULATIONS] UR DLX A50X 5P4MX UBOT v3.1.1.11 beta6 IS READY! 💥💥
+💥💥 [CONGRATULATIONS] UR DLX A50X 5P4MX UBOT v3.1.1.11 beta7 IS READY! 💥💥
 💥💥 Modded Code By @AlphaxProject Team 💥💥"""
 
 print(text)
