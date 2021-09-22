08893069b4f74cbd61648823ad8fff89dad0f87c
_XXZZ00ZZXX_ <71839916+UserXTester@users.noreply.github.com>
2021-09-22 19:29:43 +0700

    fix
    beta36.5
    - fix import, func

--- sameer.py
+++ sameer.py
@@ -8,7 +8,7 @@
 #
 # Created by : https://t.me/AlphaXProject 
 # Support by : https://t.me/CariTemanLink 
-# Version : v3.1.1.11 beta36
+# Version : v3.1.1.11 beta36.5
 
 
 import os
@@ -241,7 +241,7 @@ async def start_yukki():
     global fvt  
 
 
-    print("\n💥💥 5P4MX UBOT v3.1.1.11 beta36 IS STARTING... 💥💥\n")
+    print("\n💥💥 5P4MX UBOT v3.1.1.11 beta36.5 IS STARTING... 💥💥\n")
     
     
     if smex:
@@ -4272,6 +4272,7 @@ import os
 import os.path
 import time
 from os.path import exists, isdir
+from typing import Optional, Union
 
 # from userbot.events import register
 # from userbot.utils import humanbytes
@@ -4807,7 +4808,7 @@ async def help(e):
 For more help regarding usage \nof plugins type plugins name
 
 🤖 𝘽𝙤𝙩 𝙄𝙣𝙛𝙤 
-- version : <code>v3.1.1.11 beta36</code>
+- version : <code>v3.1.1.11 beta36.5</code>
 - type \t\t: <code>DLX</code>
 - project : <code>@AlphaXProject</code>"""
        await e.reply(text, parse_mode='html', link_preview=None )
@@ -4818,7 +4819,7 @@ For more help regarding usage \nof plugins type plugins name
         
 text = """
 
-💥 [CONGRATULATIONS] UR DLX A50X 5P4MX UBOT v3.1.1.11 beta36 IS READY! 💥
+💥 [CONGRATULATIONS] UR DLX A50X 5P4MX UBOT v3.1.1.11 beta36.5 IS READY! 💥
 💥 Modded Code By @AlphaxProject Team 💥"""
 
 print(text)
